ff1e88a869f5d225125e092db75424ac3afaece3
Daryl Hewett <dazza1308@gmail.com>
2021-10-22 21:58:28 +0100

    headertest

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>I am a typescript react headear</h1>
+        </div>
+    )
+}
+
+export default Header
