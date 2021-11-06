d36668d31d23522a4d242434bbeba5745b7a5f96
Daryl Hewett <dazza1308@gmail.com>
2021-11-06 19:49:08 +0000

    Remove test button component and variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Button from "./components/Button";
 import MessageBoard from "./components/MessageBoard";
-import { useState } from "react";
 
 function App() {
-  const [testVar, updateTestVar] = useState("Initial value");
-
-  const testFunc = function (input: string): string {
-    let output = `Button clicked - ${input}`;
-    updateTestVar("Modified");
-    return output;
-  };
-
   return (
     <div className="App">
-      <Header content="Property Header" />
-      <Button buttonText="HELLO WORLD" testFunc={testFunc} />
-      {testVar}
+      <Header content="Message Board" />
       <hr />
       <MessageBoard />
     </div>
